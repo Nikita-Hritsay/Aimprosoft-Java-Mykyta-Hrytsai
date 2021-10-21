b4eef6a36126e63222b26676eb0fb5bf489f6443
Nikita-Hritsay <hritsaynikita@gmail.com>
2021-10-21 10:54:44 +0300

    formating

--- src/main/java/com/aimprosoft/aimlearning/exceptions/DBException.java
+++ src/main/java/com/aimprosoft/aimlearning/exceptions/DBException.java
@@ -1,6 +1,6 @@
 package com.aimprosoft.aimlearning.exceptions;
 
-public class DBException extends Exception{
+public class DBException extends Exception {
 
     public DBException(String message) {
         super(message);
