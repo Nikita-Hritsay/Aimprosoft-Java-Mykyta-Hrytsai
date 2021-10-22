63dce9f319c0a81d4e130776985181c0ed3394a5
Nikita-Hritsay <hritsaynikita@gmail.com>
2021-10-22 11:05:46 +0300

    updates

--- src/main/java/com/aimprosoft/aimlearning/commands/department/DisplayAllDepartmentCommand.java
+++ src/main/java/com/aimprosoft/aimlearning/commands/department/DisplayAllDepartmentCommand.java
@@ -2,14 +2,12 @@ package com.aimprosoft.aimlearning.commands.department;
 
 import com.aimprosoft.aimlearning.commands.ICommand;
 import com.aimprosoft.aimlearning.exceptions.DBException;
-import com.aimprosoft.aimlearning.models.Department;
 import com.aimprosoft.aimlearning.services.Impl.DepartmentServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public class DisplayAllDepartmentCommand implements ICommand {
 
@@ -17,8 +15,7 @@ public class DisplayAllDepartmentCommand implements ICommand {
 
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, DBException {
-        List<Department> departments = departmentService.getAllDepartments();
-        request.setAttribute("departments", departments);
+        request.setAttribute("departments", departmentService.getAllDepartments());
         request.getRequestDispatcher("/WEB-INF/pages/homePage.jsp").forward(request, response);
     }
 
--- src/main/java/com/aimprosoft/aimlearning/commands/employee/DisplayEmployeeCommand.java
+++ src/main/java/com/aimprosoft/aimlearning/commands/employee/DisplayEmployeeCommand.java
@@ -2,14 +2,12 @@ package com.aimprosoft.aimlearning.commands.employee;
 
 import com.aimprosoft.aimlearning.commands.ICommand;
 import com.aimprosoft.aimlearning.exceptions.DBException;
-import com.aimprosoft.aimlearning.models.Employee;
 import com.aimprosoft.aimlearning.services.Impl.EmployeeServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public class DisplayEmployeeCommand implements ICommand {
 
@@ -17,8 +15,7 @@ public class DisplayEmployeeCommand implements ICommand {
 
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, DBException {
-        List<Employee> employees = employeeService.getAllEmployees();
-        request.setAttribute("employees", employees);
+        request.setAttribute("employees", employeeService.getAllEmployees());
         request.getRequestDispatcher("/WEB-INF/pages/allEmployees.jsp").forward(request, response);
     }
 }
