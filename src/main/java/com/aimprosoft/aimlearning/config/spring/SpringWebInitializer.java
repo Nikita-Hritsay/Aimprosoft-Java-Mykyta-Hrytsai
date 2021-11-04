bd1bc4b1d759536d57e105aaac47f12bbfca8e2f
Nikita-Hritsay <hritsaynikita@gmail.com>
2021-11-04 11:51:36 +0200

    adding spring config

---
+++ src/main/java/com/aimprosoft/aimlearning/config/spring/SpringWebInitializer.java
@@ -0,0 +1,24 @@
+package com.aimprosoft.aimlearning.config.spring;
+
+import com.aimprosoft.aimlearning.controllers.MainController;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class SpringWebInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(MainController.class);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
+                "MainController", new DispatcherServlet(appContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+
+    }
+}
