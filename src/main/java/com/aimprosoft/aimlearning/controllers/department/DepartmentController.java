eff29a2a2d3c772d6f50c59081ed7d3e09662fbc
Nikita-Hritsay <hritsaynikita@gmail.com>
2021-11-11 14:32:43 +0200

    adding not working controllers

--- src/main/java/com/aimprosoft/aimlearning/controllers/department/DepartmentController.java
+++ src/main/java/com/aimprosoft/aimlearning/controllers/department/DepartmentController.java
@@ -1,7 +1,23 @@
 package com.aimprosoft.aimlearning.controllers.department;
 
+import com.aimprosoft.aimlearning.exceptions.DBException;
+import com.aimprosoft.aimlearning.services.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @GetMapping("/")
+    public String helloPage(Model model) throws DBException {
+        model.addAttribute("departments", departmentService.getAllDepartments());
+        return "helloWorld";
+    }
+
 }
