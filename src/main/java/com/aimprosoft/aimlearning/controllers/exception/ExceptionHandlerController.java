979f980fe5a1b8cd4587c79a4f713137f2345c4b
Nikita-Hritsay <hritsaynikita@gmail.com>
2021-11-17 10:57:42 +0200

    code formatting

--- src/main/java/com/aimprosoft/aimlearning/controllers/exception/ExceptionHandlerController.java
+++ src/main/java/com/aimprosoft/aimlearning/controllers/exception/ExceptionHandlerController.java
@@ -1,17 +1,18 @@
 package com.aimprosoft.aimlearning.controllers.exception;
 
 import com.aimprosoft.aimlearning.exceptions.DBException;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 @ControllerAdvice
-class GlobalControllerExceptionHandler {
+class ExceptionHandlerController {
+
     @ExceptionHandler(DBException.class)
     public String handleConflict(Model model) {
         model.addAttribute("error", "DB exception");
         return "errorPage";
     }
+
 }
