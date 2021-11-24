2abeb99906101f4b2aa8699ec8bb136eedc04da4
Nikita-Hritsay <hritsaynikita@gmail.com>
2021-11-24 13:30:15 +0200

    adding production webpack config:

--- web/js/webpack.prod.config.js
+++ web/js/webpack.prod.config.js
@@ -1,34 +1,8 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.config.js');
 
-module.exports = {
-    entry: "./src/index.js",
-    mode: "production",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                loader: "ts-loader",
-                exclude: ["/node_modules/"],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: "asset",
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-}
-
+module.exports = merge(common,{
+    mode: "production"
+});
 
 
