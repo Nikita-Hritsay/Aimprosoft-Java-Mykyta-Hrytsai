f84eef52c1bd6f3d2a0e33873f637338754298e9
Nikita-Hritsay <hritsaynikita@gmail.com>
2021-11-24 18:18:53 +0200

    adding ts DepartmentList.ts

--- web/js/src/components/departmentList/Department.ts
+++ web/js/src/components/departmentList/Department.ts
@@ -1,9 +1,12 @@
-function componentTs() {
-    const element = document.createElement('div');
-
-    console.log("TypeScript")
-
-    return element;
+function component() {
+    $.getJSON(
+        'http://localhost:8080/emplpoyee'
+    ).then(function (result) {
+        console.log('result', result)
+        document.getElementById("app").innerHTML = result[0]
+    }).catch(function (err) {
+        console.log('err', err)
+    })
 }
 
-document.body.appendChild(componentTs());
+component()
