0e6ea11931dc7c0f4d3e211b45ce03b628ccfca7
Nikita-Hritsay <hritsaynikita@gmail.com>
2021-12-01 13:36:42 +0200

    updating employee list des

--- web/js/src/components/employee/EmployeeList.ts
+++ web/js/src/components/employee/EmployeeList.ts
@@ -10,7 +10,8 @@ export class EmployeeList implements Component{
     render(param: number){
         if(param != null){
             this.employeeService.getEmployees().done((data) => {
-                if (data != null){
+                let employees = this.getData(param, data);
+                if (employees != null){
                     const main = $("#main");
                     main.empty();
                     const table =  $("<table/>");
@@ -23,34 +24,30 @@ export class EmployeeList implements Component{
                     headerTable.append($("<th/>", {text: "Update"}));
                     headerTable.append($("<th/>", {text: "Delete"}));
                     table.append(headerTable);
-                    for(let i = 0; i < data.length; i++){
-                        if(data[i].department.id == param || param == 0){
-                            const td = $("<tr/>");
-                            td.append($("<td/>", {text: data[i].firstName})); 
-                            td.append($("<td/>", {text: data[i].lastName})); 
-                            td.append($("<td/>", {text: data[i].email}));
-                            td.append($("<td/>", {text: data[i].salary}));
-                            td.append($("<td/>", {text: new Date(data[i].hireDate).toISOString().slice(0, 10)})); 
-                            const updateButton = $("<a />", {text: "update"}).addClass("update_button");
-                            td.append($("<td/>").append(updateButton.on("click", () => {
-                                new Router().getUrl("#employeeForm").render(data[i]);
-                            })))
-
-                            const deleteButton = $("<button />", {text: "delete"}).addClass("delete_button").addClass("submit_delete");
-                            td.append($("<td/>").append(deleteButton.on("click", () => {
-                                this.employeeService.deleteEmployee(data[i].id).done(()=>{
-                                    new Router().getUrl("#employee").render(param);
-                                })
-                            })))
-                            table.append(td);
-                            main.append(table);
-                        }
-                        
+                    for(let i = 0; i < employees.length; i++) {
+                        const td = $("<tr/>");
+                        td.append($("<td/>", {text: employees[i].firstName})); 
+                        td.append($("<td/>", {text: employees[i].lastName})); 
+                        td.append($("<td/>", {text: employees[i].email}));
+                        td.append($("<td/>", {text: employees[i].salary}));
+                        td.append($("<td/>", {text: new Date(employees[i].hireDate).toISOString().slice(0, 10)})); 
+                        const updateButton = $("<a />", {text: "update"}).addClass("update_button");
+                        td.append($("<td/>").append(updateButton.on("click", () => {
+                            new Router().getUrl("#employeeForm").render(employees[i]);
+                        })))
+                        const deleteButton = $("<button />", {text: "delete"}).addClass("delete_button").addClass("submit_delete");
+                        td.append($("<td/>").append(deleteButton.on("click", () => {
+                            this.employeeService.deleteEmployee(employees[i].id).done(()=>{
+                                new Router().getUrl("#employee").render(param);
+                            })
+                        })))
+                        table.append(td);
+                        main.append(table);
                     }
                 }else{
                     const main = $("#main");
                     main.empty();
-                    main.append($("<p />", {text: "There is no employees"}))
+                    main.append($("<p />", {text: "There is no employees in this Department"}))
                 }
             });
         }
@@ -58,5 +55,14 @@ export class EmployeeList implements Component{
             const main = $("#app");
             main.append($("<p />", {text: "there is no such Deparment"}));
         }
+    }
+
+    private getData(param: number, data: any): any{
+        let employees = data.filter((el: any) => el.department.id == param);
+        if(employees.length > 0)
+            return employees;
+        if(param == 0)
+            return data;
+        return null;
     }
 }
