13e1dd259429774497fc22a12f93b2c193d02636
Nikita-Hritsay <hritsaynikita@gmail.com>
2021-12-15 11:00:01 +0200

    updating

--- web/src/container/employee/DisplayEmployeesByDepartment.ts
+++ web/src/container/employee/DisplayEmployeesByDepartment.ts
@@ -1,16 +1,22 @@
 import { EmployeeByDepartment } from "../../components/employee/EmployeeByDepartment";
 import { EmployeeService } from "../../service/EmployeeService";
+import {DataNotFoundPage} from "../../components/error/DataNotFoundPage";
+import {DepartmentService} from "../../service/DepartmentService";
 
 
 export class DisplayEmployeesByDepartment {
     private employeeService = new EmployeeService();
     private employeeComponent = new EmployeeByDepartment();
+    private dataNotFound = new DataNotFoundPage();
+    private departmentService = new DepartmentService();
 
     render(param: number){
-        this.employeeService.getByDepartment(param).done((data)=>{
-            this.employeeComponent.render(data, param);
+        this.departmentService.getDepartmentById(param).done(()=>{
+            this.employeeService.getByDepartment(param).done((data)=>{
+                this.employeeComponent.render(data, param);
+            })
         }).fail(()=>{
-            this.employeeComponent.render(null, null);
+            this.dataNotFound.render();
         });   
     }
 
