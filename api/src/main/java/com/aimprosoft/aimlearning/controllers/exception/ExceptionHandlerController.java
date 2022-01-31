8255ab3d1a87479f364778dfc746f9be992081f0
Nikita-Hritsay <hritsaynikita@gmail.com>
2022-01-31 17:34:55 +0200

    updating

--- api/src/main/java/com/aimprosoft/aimlearning/controllers/exception/ExceptionHandlerController.java
+++ api/src/main/java/com/aimprosoft/aimlearning/controllers/exception/ExceptionHandlerController.java
@@ -1,7 +1,6 @@
 package com.aimprosoft.aimlearning.controllers.exception;
 
 import com.aimprosoft.aimlearning.exceptions.DBException;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
